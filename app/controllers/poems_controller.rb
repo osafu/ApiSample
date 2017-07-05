98f9099f09d0e9b849075e57c7129c4276557684
長船託也 <osa929@gmail.com>
2017-07-05 18:03:18 +0900

    DIVE14 課題対応5

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -5,8 +5,8 @@ class PoemsController < ApplicationController
   end
 
   def show
-    @poems = Poem.find(params[:id])
-    render json: @poems
+    @poem = Poem.find(params[:id])
+    render json: @poem
   end
 
 end
